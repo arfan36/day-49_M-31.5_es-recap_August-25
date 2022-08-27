3ccc3622d4223726bd132c5117f7c18e7ab6476a
arfan36 <arfanur36@gmail.com>
2022-08-27 17:58:06 +0600

    first commit

---
+++ 1_recap.js
@@ -0,0 +1,35 @@
+/* 
+Must know
+1. var, let, const (alrady practiced)
+2. template string
+    2.1: create multi-ling string(` `)
+    2.2: add dynamic variable (expressing): `${}`
+3. Arrow function
+    3.1: single parameter arrow function
+    3.2: two parameters arrow function
+    3.3: arrow function with function body. And dont forget you have to to returne explicitly
+4. map, filter, find
+5. optional chaining
+ */
+
+// GOOD TO HaVE
+/* 
+    1. spread operator: [...numbers, 12, 45]
+    2. destructuring
+        2-1: object destructuring. how to get property name as variable
+            const {x} = {x:3, y:5}
+        2-2: array destructuring
+            const [a, b] = [3, 8, 12, 56]
+        
+    3. default parameter
+    4. forEach
+    5. dot notation vs bracket notation
+ */
+
+// SHOULD KNOW THE name
+/* 
+1. reduce
+2. inheritance
+3. prototypical chain
+4. class, object
+ */
